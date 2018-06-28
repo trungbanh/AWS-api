cfb3fb99e955973c9ebd5627036eb190a4938b9a
lequangsang97 <sang.lequang97@gmail.com>
2018-06-28 17:18:21 +0000

    upload collection

---
+++ list_collection.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./config.json');
+
+AWS.config.update({ region: 'us-east-1' });
+var rekognition = new AWS.Rekognition({});
+
+var params = {
+  CollectionId: "faces",
+  MaxResults: 1000
+};
+rekognition.listFaces(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else {
+    console.log(data);
+    console.log(data.Faces.length)
+
+  }
+});
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const AWS = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const cors = require('cors');
+const port = process.env.PORT || 3001;
 
 //config aws
 AWS.config.loadFromPath('./config.json');
-
 
 // create express app
 const app = express();
@@ -24,39 +24,63 @@ app.get('/', (req, res) => {
   });
 })
 const s3 = new AWS.S3();
-
-const port = process.env.PORT || 3001;
+var rekognition = new AWS.Rekognition({});
 // upload
-let aPromise = new Promise((resolve, reject) => {
-  resolve(f1());
-});
 const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'awsrek',
     metadata: function (req, file, cb) {
-      console.log(req.body)
       cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
-      console.log(req);
-      console.log(file);
       const keyFile = file.originalname;
-      cb(null, `${Date.now().toString()}-${keyFile}`)
+      //   cb(null, `${Date.now().toString()}-${keyFile}`)
+      cb(null, `${keyFile}`)
     }
   })
 });
-let f1 = () => {
-  app.post('/upload', upload.single('iamge'), (req, res) => {
-    console.log(req.file)
-    res.json({
-      message: 'upload sucess'
-    })
+
+app.post('/upload', upload.single('image'), (req, res) => {
+  // params add faces 
+  var params = {
+    "CollectionId": "faces",
+    "Image": {
+      "S3Object": {
+        "Bucket": "awsrek",
+        "Name": req.file.originalname
+      }
+    },
+    "ExternalImageId": req.file.originalname,
+    "DetectionAttributes": [
+      "ALL"
+    ]
+  }
+  rekognition.indexFaces(params, (err, data) => {
+    if (err) {
+      console.log(err.toString());
+    } else {
+      if (data.FaceRecords.length === 0) {
+        res.json({
+          message: 'Faces not found. Please try again'
+        })
+      }
+      else if (data.FaceRecords.length > 1) {
+        res.json({
+          message: 'Please Take only face. Please try again',
+          FaceId: data.FaceRecords
+        })
+      } else {
+        res.json({
+          Message: 'upload sucess',
+          File: req.file.originalname,
+          FaceId: data.FaceRecords[0].Face.FaceId,
+          ImageId: data.FaceRecords[0].Face.ImageId,
+          ExternalImageId: data.FaceRecords[0].Face.ExternalImageId
+        });
+        console.log(data);
+      }
+    }
   })
-}
-aPromise.then((result) => {
-  console.log('thanh cong ye ye');
-}).catch((err) => {
-  
-});
+})
 app.listen(port, () => console.log(`server running on port ${port}`));
